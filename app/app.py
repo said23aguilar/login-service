141cd98d2108312c9ca07ebea2953e37b637c9b5
Said Aguilar <78769950+said23aguilar@users.noreply.github.com>
2024-05-28 10:36:43 -0600

    bd

--- app/app.py
+++ app/app.py
@@ -50,5 +50,6 @@ with app.app_context():
         create_database(DB_URL)
     db.create_all()
 
+port = int(os.environ.get('PORT', 8080))
 if __name__ == "__main__":
-    app.run(host=os.getenv("HOST"), debug=True, port=os.getenv("PORT"))
+    app.run(threaded=True, host='0.0.0.0', port=port)
